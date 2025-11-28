bc9ee7fad7ccb5d8408deac6230dd50b75d6513e
nrep-ug <derrickmaiku@gmail.com>
2025-11-28 16:10:13 +0300

    fix: clean up ESLint unescaped entities

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.APPWRITE_ENDPOINT,
-    NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.APPWRITE_PROJECT_ID,
-  },
   eslint: {
     // Allow production builds to complete even if there are ESLint errors
     ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.APPWRITE_ENDPOINT,
+    NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.APPWRITE_PROJECT_ID,
   },
 };
 
