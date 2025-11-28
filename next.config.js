fa20722a41f2055ca4fb584d1cb458dd94ff169c
nrep-ug <derrickmaiku@gmail.com>
2025-11-28 14:20:18 +0300

    chore: ignore ESLint during production builds

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.APPWRITE_ENDPOINT,
     NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.APPWRITE_PROJECT_ID,
   },
+    eslint: {
+    // Allow production builds to complete even if there are ESLint errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
